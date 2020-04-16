74662f72d0fc250bd0bb26e664bc5763533bb486
StuartSpiegel <48696059+StuartSpiegel@users.noreply.github.com>
2020-04-16 14:37:12 -0400

    - Trixie 1.3-0 First Feature push - --working on reading directly from wiki--

--- readWiki.py
+++ readWiki.py
@@ -1,5 +1,7 @@
 # Trixie Version 1.3-0 adding feature to read directly from wiki as opposed to formatted text file
-# This Update requires Python 3.4.0 or later
+# **This Update requires Python 3.4.0 or later**
+# Either store the parsed wiki data as a template for operations or possibly a wrapper , trying to avoid
+# writing/saving files
 
 import wikitextparser as wtp
 from wikitextparser import WikiLink
@@ -41,3 +43,50 @@ t.rm_dup_args_safe()
 params = wtp.parse('{{a|b}}').parameters[0]
 
 # TODO: Get and print the parsed sections with proper formatting
+parsed_sections = wtp.parse("""
+... == h2 ==
+... t2
+... === h3 ===
+... t3
+... === h3 ===
+... t3
+... == h22 ==
+... t22
+... {{text|value3}}
+... [[Z|X]]
+... """)
+print(parsed)
+# Strip the title line
+del parsed.sections[1].title
+
+# Getting list and table data from a wiki format
+p = wtp.parse("""{|
+... |  Orange    ||   Apple   ||   more
+... |-
+... |   Bread    ||   Pie     ||   more
+... |-
+... |   Butter   || Ice cream ||  and more
+... |}""")
+
+table_data = p.tables[0].data()
+# The default setting is arrangement according to column span and row span attrib
+table_data_NOSPAN = p.tables[0].data(span=False)
+
+# Get the list data and sub-list data
+list = {"list item 1", "list item 2", "list item 3"}
+parsed_list = wtp.parse(list)
+
+wikilist = parsed.get_lists()[0]
+print(wikilist.items)
+
+sublists = wikilist.sublists(1)[0].items
+print(wikilist.sublists(1)[0].items)
+
+# HTML Tags
+ref, references = p.get_tags()
+ref.name = "X_NAME"
+
+# Getting parent and child node data | searching for ancestors of a given type
+template_1 = wtp.parse("{{a|{{b|{{c|{{d}}}}}}}}").templates[3]
+template_ancestors = template_1.ancestors()
+template_parents = template_1.parent()
